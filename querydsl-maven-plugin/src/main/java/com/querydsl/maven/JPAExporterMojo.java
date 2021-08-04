1eb505db925bdc794c544929acca09b8bd30f6fe
thong.nguyen <thanhthong022002@gmail.com>
2021-08-04 18:40:00 +0700

    querydsl-maven-plugin#jpa-exporter - Allow config annotation helpers - Allow config typeMappingsClass

--- querydsl-maven-plugin/src/main/java/com/querydsl/maven/JPAExporterMojo.java
+++ querydsl-maven-plugin/src/main/java/com/querydsl/maven/JPAExporterMojo.java
@@ -13,10 +13,13 @@
  */
 package com.querydsl.maven;
 
-import javax.persistence.*;
-
+import com.querydsl.codegen.AnnotationHelper;
 import com.querydsl.codegen.GenericExporter;
 import com.querydsl.codegen.PropertyHandling;
+import com.querydsl.codegen.TypeMappings;
+
+import javax.persistence.*;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * {@code JPAExporterMojo} calls {@link GenericExporter} using the classpath of the module
@@ -26,6 +29,19 @@ import com.querydsl.codegen.PropertyHandling;
  * @author tiwe
  */
 public class JPAExporterMojo extends AbstractExporterMojo {
+
+    /**
+     * annotation helpers for exporter
+     *
+     * @parameter
+     */
+    private String[] annotationHelpers;
+    /**
+     * custom typeMappings class
+     *
+     * @parameter
+     */
+    private String typeMappingsClass;
 
     @Override
     protected void configure(GenericExporter exporter) {
@@ -39,5 +55,45 @@ public class JPAExporterMojo extends AbstractExporterMojo {
 
         // AnnotationHelpers to process specific JPA annotations
         exporter.addAnnotationHelper(JPATemporalAnnotationHelper.INSTANCE);
+
+        if (this.typeMappingsClass != null) {
+            try {
+                exporter.setTypeMappingsClass(
+                        (Class<? extends TypeMappings>) Class.forName(typeMappingsClass)
+                );
+            } catch (ClassNotFoundException e) {
+                getLog()
+                        .error("Unknown typeMappings: " + typeMappingsClass);
+            }
+        }
+
+        // add custom annotation helper
+        if (annotationHelpers != null && annotationHelpers.length > 0) {
+            for (String annotationHelper : annotationHelpers) {
+                try {
+                    Class<?> clazz = Class.forName(annotationHelper);
+                    if (AnnotationHelper.class.isAssignableFrom(clazz)) {
+                        AnnotationHelper obj = (AnnotationHelper) clazz.getDeclaredConstructor().newInstance();
+                        exporter.addAnnotationHelper(obj);
+                        getLog().info("Register annotation helper: " + annotationHelper);
+                    } else {
+                        getLog().error("Clazz " + annotationHelper +
+                                " is not a AnnotationHelper");
+                    }
+                } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                    getLog().error("Can't init helper: " + e.getMessage());
+                    return;
+                }
+            }
+        }
+    }
+
+    public void setAnnotationHelpers(String[] annotationHelpers) {
+        this.annotationHelpers = annotationHelpers;
+    }
+
+    public void setTypeMappingsClass(String typeMappingsClass) {
+        this.typeMappingsClass = typeMappingsClass;
     }
 }
