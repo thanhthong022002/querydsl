8da3b7b525a4ff9e4802f3dcc5c0cb9407844ba8
thong.nguyen <thanhthong022002@gmail.com>
2021-08-05 14:25:28 +0700

    Custom version 10.0.1

--- release-xo.bat
+++ release-xo.bat
@@ -1,1 +1,9 @@
-mvn -Pxomad clean deploy "-Denforcer.skip=true" -DskipTests "-Dcheckstyle.skip=true"
+set VERSION=%1
+set BRANCH="xomad-%VERSION%"
+
+CALL mvn "-Pall,docs" versions:set -DgenerateBackupPoms=false "-DnewVersion=%VERSION%"
+CALL mvn -Pxomad clean package "-Denforcer.skip=true" -DskipTests "-Dcheckstyle.skip=true"
+
+CALL git checkout -b %BRANCH%
+CALL git add .
+CALL git commit -m "Custom version %VERSION%"
