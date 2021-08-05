8ff886fd9e436f2a6d946cb8563f61f3017d4061
thong.nguyen <thanhthong022002@gmail.com>
2021-08-05 14:51:09 +0700

    deploy instead of package

--- release-xo.bat
+++ release-xo.bat
@@ -2,7 +2,7 @@ set VERSION=%1
 set BRANCH="xomad-%VERSION%"
 
 mvn "-Pall,docs" versions:set -DgenerateBackupPoms=false "-DnewVersion=%VERSION%" && ^
-mvn -Pjpa clean package -am "-Denforcer.skip=true" -DskipTests "-Dcheckstyle.skip=true" && ^
+mvn -Pjpa clean deploy -am "-Denforcer.skip=true" -DskipTests "-Dcheckstyle.skip=true" && ^
 git checkout -b %BRANCH% && ^
 git add . && ^
 git commit -m "Custom version %VERSION%"
